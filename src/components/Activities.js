1f6cb2fb9185eb8326c4604411720ef673650519
PoojithaMiriyala <76590219+PoojithaMiriyala@users.noreply.github.com>
2023-05-12 15:35:08 +0530

    Added Activites and Placement

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -7,6 +7,7 @@ const ActivitiesContainer = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
+  width: 50%;
 `;
 
 const ActivitiesText = styled.h1`
@@ -16,11 +17,38 @@ const ActivitiesText = styled.h1`
   margin-top: 2rem;
 `;
 
+const Headline = styled.h2`
+  font-size: 1.5rem;
+  color: #FF0000;
+`;
+const SimpleText = styled.div`
+  font-size: 0.5rem;
+  color: #000000;
+`;
+
+
 const Activities = () => {
   return (
+    <div style={{display: "flex", justifyContent:"center"}}>
     <ActivitiesContainer>
       <ActivitiesText>Activities</ActivitiesText>
+
+      <Headline>Workshop on Crafting Research</Headline> 
+      <SimpleText>17.12.20 to 19.12.20</SimpleText>
+      <SimpleText>Dr. Shailendra Kumar,Dr. Utkarsh Goel and Dr. Parikshit Joshi</SimpleText>
+      <SimpleText>Scientific Research Methods helps a researcher in exploring better insights of the problem or situation. The 3-Day Workshop aims at inculcating the concept of Scientific Research amongst the researchers by focusing of the philosophical aspects of theory building and statistical aspects related to theory validation. The Workshop also covers several multivariate data analysis techniques from the experts. Proper hands on with area experts will also be provided to the participants.</SimpleText>
+
+      <Headline>Workshop on “Entrepreneurship Development”</Headline>
+      <Headline>Five days FDP Workshop on “Creative problem solving and Critical thinking”</Headline>
+      <Headline>ATAL sponsored FDP Programme on “Emotional Intelligence”</Headline>
+      <Headline>“Colloquium on Information Flux and its impact on Societal Resilience : A case of conflict or concord”</Headline>
+      <Headline>Workshop on “Innovation Management and Entrepreneurship Development”</Headline>
+      <Headline>Third Summer School on “Behavioral Finance”</Headline>
+      <Headline>A talk on Digital and IT Transformations into decision making.</Headline>
+      <Headline>Guest Lecture by Prof. D.K. Pandiya</Headline>
+      <Headline>Workshop on Leadership by Mrs. Yashodhara Raj</Headline>
     </ActivitiesContainer>
+    </div>
   );
 };
 
--- src/components/Placements.js
+++ src/components/Placements.js
@@ -19,7 +19,11 @@ const PlacementsText = styled.h1`
 const Placements = () => {
   return (
     <PlacementsContainer>
-      <PlacementsText>Placements</PlacementsText>
+      <PlacementsText>
+        <h2>Training & Placements Officer's Message</h2>
+        <p>The business environment today requires efficient handling of complex, highly unpredictable situations. By providing an excellent mix of highly qualified faculty and unmatched course content, IIIT-Allahabad ensure that our students are well prepared to step into this volatile corporate world and occupy positions of stature. We not only work for placing students to various companies but we also ensure their overall growth. I wish a very successful career ahead to all the students of IIIT-A and feel confident that they will make significant contributions to the organizations they join. It is a matter of privilege to us that you have shown considerable interest in the MBA program of the institute. We here in IIIT-A take it as a matter of pride and great responsibility to be an institute of national importance.
+</p>
+      </PlacementsText>
     </PlacementsContainer>
   );
 };
